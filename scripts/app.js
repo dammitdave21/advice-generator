654ac62ef1d4324dd4f52afa979b5ed4deac9103
dammitdave21 <dammitdave24@gmail.com>
2024-01-29 11:28:09 +0900

    this is the first commit

---
+++ scripts/app.js
@@ -0,0 +1,32 @@
+// Get all the elements
+const dice = document.querySelector('.dice');
+const adviceNum = document.querySelector('.advice-number');
+const advice = document.querySelector('.advice');
+
+const getAdvice = () => {
+   const req = new XMLHttpRequest();
+   const src = `https://api.adviceslip.com/advice?timestamp=${Date.now()}`;
+
+   req.addEventListener('readystatechange', () => {
+
+      if (req.readyState === 4 && req.status === 200) {
+
+         const res = JSON.parse(req.response);
+
+         adviceNum.textContent = `Advice #${res.slip.id}`;
+         advice.textContent = `"${res.slip.advice}"`;
+
+      } else if (req.readyState === 4) {
+         console.log('Could not get the data!');
+      }
+
+   });
+
+   req.open('GET', src);
+   req.send();
+
+}
+
+dice.addEventListener('click', getAdvice);
+
+
